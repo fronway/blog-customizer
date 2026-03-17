acb9e1dd6cd8dac645e122292782715238f2b749
Cret1noo <creat1noo@yandex.com>
2026-03-17 23:50:08 +0300

    feat: add article settings form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,87 @@
+import { useState, useRef, useEffect } from 'react';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	contentWidthArr,
+	backgroundColors,
+	ArticleStateType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const sideBarRef = useRef<HTMLElement>(null);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	useEffect(() => {
+		if (!isSidebarOpen) return;
+		const handleClickOutside = (e: MouseEvent) => {
+			if (
+				sideBarRef.current &&
+				!sideBarRef.current.contains(e.target as Node)
+			) {
+				setFormState(false);
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isSidebarOpen]);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isSidebarOpen}
+				onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+			/>
+			<aside className={styles.container} ref={sideBarRef}>
 				<form className={styles.form}>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={handleChange('fontFamilyOption')}
+					/>
+					<Select
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={handleChange('fontSizeOption')}
+					/>
+					<Separator />
+
+					<RadioGroup
+						name='fontColor'
+						options={fontColors}
+						selected={formState.fontColor}
+						title='Цвет шрифта'
+						onChange={handleChange('fontColor')}
+					/>
+					<Separator />
+
+					<RadioGroup
+						name='backgroundColor'
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						title='Цвет фона'
+						onChange={handleChange('backgroundColor')}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={handleChange('contentWidth')}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
