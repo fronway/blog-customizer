fd3b18573424459c2cd13592fd232d92fd816d7b
Cret1noo <creat1noo@yandex.com>
2026-03-19 13:09:02 +0300

    Готовый проект

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,117 @@
+import { FormEvent, useState, useRef, useEffect } from 'react';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	contentWidthArr,
+	backgroundColors,
+	ArticleStateType,
+	defaultArticleState,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onApply: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ onApply }: ArticleParamsFormProps) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const sideBarRef = useRef<HTMLElement | null>(null);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleChange =
+		(key: keyof ArticleStateType) => (selected: OptionType | null) => {
+			if (!selected) return;
+			setFormState((prev) => ({
+				...prev,
+				[key]: selected,
+			}));
+		};
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onApply(formState);
+	};
+	const handleReset = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setFormState(defaultArticleState);
+		onApply(defaultArticleState);
+	};
+	useEffect(() => {
+		if (!isSidebarOpen) return;
+		const handleClickOutside = (e: MouseEvent) => {
+			if (
+				sideBarRef.current &&
+				!sideBarRef.current.contains(e.target as Node)
+			) {
+				setIsSidebarOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isSidebarOpen]);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isSidebarOpen}
+				onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+			/>
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isSidebarOpen,
+				})}
+				ref={sideBarRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={handleChange('fontFamilyOption')}
+					/>
+					<Select
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={handleChange('fontSizeOption')}
+					/>
+					<Separator />
+
+					<RadioGroup
+						name='fontColor'
+						options={fontColors}
+						selected={formState.fontColor}
+						title='Цвет шрифта'
+						onChange={handleChange('fontColor')}
+					/>
+					<Separator />
+
+					<RadioGroup
+						name='backgroundColor'
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						title='Цвет фона'
+						onChange={handleChange('backgroundColor')}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={handleChange('contentWidth')}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
